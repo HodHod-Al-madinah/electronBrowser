07a60bf09112d0f1a470a05c9609d43492be4e47
hammam alageed <hammamalageed@gmail.com>
2024-10-24 20:20:39 +0300

    before add watsapp

--- utils/settings.js
+++ utils/settings.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
-let settingsFile = path.join(__dirname, '../../settings.json');
+let settingsFile = path.join(__dirname, '../settings.json');
+
 // Function to load settings from the settings.json file
 function loadSettings() {
   try {
@@ -11,16 +12,17 @@ function loadSettings() {
     return settings.scaleFactor || 88;  // Return scaleFactor or default value
   } catch (error) {
     console.log('No settings file found, using defaults.');
-    return 88;  // Default scale factor   
+    return 88;  // Default scale factor
   }
 }
 
-
 // Function to save the current scaleFactor to the settings.json file
-function saveSettings() {
+function saveSettings(scaleFactor) {  // Pass scaleFactor as a parameter
   const settings = {
     scaleFactor: scaleFactor,
   };
   fs.writeFileSync(settingsFile, JSON.stringify(settings, null, 2));
+  console.log('Settings saved:', settings);
 }
+
 module.exports = { loadSettings, saveSettings };

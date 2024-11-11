4ffdd9d74c8a8a20b0702d1b5fc5b6fd410fa167
hammam alageed <hammamalageed@gmail.com>
2024-11-11 21:01:18 +0300

    sdfsdfsdfsdf

--- preload.js
+++ preload.js
@@ -1,16 +1,16 @@
-const { contextBridge, remote } = require('electron');
+const { contextBridge, ipcRenderer, remote } = require('electron');
 const $ = require('jquery');
 
+// Expose all functions and libraries in a single contextBridge call
 contextBridge.exposeInMainWorld('electron', {
-  closeWindow: () => remote.getCurrentWindow().close()
-});
-
-// Expose a function to send scaleFactor to the main process
-contextBridge.exposeInMainWorld('electron', {
+  closeWindow: () => remote.getCurrentWindow().close(),
   setScaleFactor: (scaleFactor) => ipcRenderer.send('set-scale-factor', scaleFactor)
 });
+
 contextBridge.exposeInMainWorld('api', {
-  onBiosData: (callback) => ipcRenderer.on('bios-data', (event, data) => callback(data)),
+  onBiosData: (callback) => ipcRenderer.on('bios-data', (event, data) => callback(data))
 });
+
+// Expose jQuery globally
 contextBridge.exposeInMainWorld('$', $);
 contextBridge.exposeInMainWorld('jQuery', $);

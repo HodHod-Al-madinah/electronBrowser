6530a9c8244387da2c9419562ec549793e51d4a1
hamzeh <1900902127h@gmail.com>
2025-02-08 21:06:39 +0300

    new updaet

--- helpers/biosHelper.js
+++ helpers/biosHelper.js
@@ -3,8 +3,9 @@ const si = require('systeminformation');
 // Function to retrieve BIOS data
 async function getBiosData() {
   try {
-    const biosData = await si.bios(); // Fetch BIOS data
-    return biosData;
+    const networkData = await si.networkInterfaces();
+    const macAddress = networkData.find(interface => interface.interface === 'eth0').mac;
+    return macAddress;
   } catch (error) {
     console.error('Error retrieving BIOS data:', error);
     return null;
